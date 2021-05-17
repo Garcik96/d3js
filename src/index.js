4f032894740de835f91e5b6b649b7c3804186c3b
Ivan Garcia Ballesteros <garcia.ballesteros96@gmail.com>
2021-05-17 21:06:15 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,5 +1,94 @@
 import './styles.scss';
 
 (() => {
+    var svg = d3.select("svg"),
+    width = +window.innerWidth,
+    height = +window.innerHeight;
+
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+  var simulation = d3.forceSimulation()
+      .force("link", d3.forceLink().distance(function(d) { return d.distance; }).id(function(d) { return d.id; }))
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2));
+
+  d3.json("./assets/nba.json", function(error, graph) {
+    if (error) throw error;
+
+    var link = svg.append("g")
+      .attr("class", "links")
+      .selectAll("line")
+      .data(graph.links)
+      .enter().append("line")
+      .attr("stroke-width", function(d) { return d.value; })
+      .attr("class", function(d) {
+        switch(d.connection) {
+          case "player":
+            return "link_player";
+          case "former teammate":
+            return "link_former_teammate";
+        }
+      });
+
+    var node = svg.append("g")
+        .attr("class", "nodes")
+        .selectAll("g")
+        .data(graph.nodes)
+        .enter().append("g")
     
+    var circles = node.append("circle")
+        .attr("r", function(d) { return d.age; })
+        .attr("fill", function(d) { return color(d.group); })
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+    var labels = node.append("text")
+        .text(function(d) {
+          return d.id;
+        })
+        .attr('x', function(d) { return -(d.age); })
+        .attr('y', function(d) { return -(d.age) - 5; });
+
+    node.append("title")
+        .text(function(d) { return d.id; });
+
+    simulation
+        .nodes(graph.nodes)
+        .on("tick", ticked);
+
+    simulation.force("link")
+        .links(graph.links);
+
+    function ticked() {
+      link
+          .attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
+
+      node
+        .attr("transform", function(d) {
+          return "translate(" + d.x + "," + d.y + ")";
+        })
+    }
+  });
+
+  function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
 })()
